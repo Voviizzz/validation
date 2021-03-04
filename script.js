3721fcfb6c17dc63edd1c29459580c67cd7f2685
Voviizzz <ivovochka48@gmail.com>
2021-03-04 17:33:41 +0300

    validatioon is worked

--- script.js
+++ script.js
@@ -1,62 +1,61 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function () {
-  const form = document.getElementById('form');
-  form.addEventListener('submit', formSend);
+  const form = document.getElementById("form");
+  form.addEventListener("submit", formSend);
 
   async function formSend(e) {
     e.preventDefault();
     let error = formValidate(form);
     let formData = new FormData(form);
     console.log(error);
-    if (error === 0){
-      form.classList.add('_sending');
-      let response = await fetch('sendmail.php', {
-        method: 'POST',
-        body: formData
+    if (error === 0) {
+      form.classList.add("_sending");
+      let response = await fetch("sendmail.php", {
+        method: "POST",
+        body: formData,
       });
-      if (response.ok){
+      if (response.ok) {
         let result = await response.json();
         alert(result.message);
         form.reset();
-        form.classList.remove('_sending');
+        form.classList.remove("_sending");
       } else {
         alert("Ошибка");
-        form.classList.remove('_sending');
+        form.classList.remove("_sending");
       }
     } else {
-    
-      alert('Заполните обязательные поля');
+      alert("Заполните обязательные поля");
+    }
   }
-}
   function formValidate(form) {
     let error = 0;
-    let formReq = document.querySelectorAll('._req');
+    let formReq = document.querySelectorAll("._req");
 
     for (let index = 0; index < formReq.length; index++) {
       const input = formReq[index];
       formRemoveError(input);
-      if (input.classList.contains('_email')) {
+      if (input.classList.contains("_email")) {
         if (validateEmail(input)) {
           formAddError(input);
           error++;
         }
       } else if (
         input.getAttribute("type") === "checkbox" &&
-        input.checked === false) {
+        input.checked === false
+      ) {
         formAddError(input);
         error++;
       } else {
-        if (input.value === '') {
+        if (input.value === "") {
           formAddError(input);
           error++;
         }
       }
-    } 
+    }
     return error;
   }
   function formAddError(input) {
-    
     input.classList.add("_error");
   }
   function formRemoveError(input) {
@@ -64,10 +63,8 @@ document.addEventListener("DOMContentLoaded", function () {
     input.classList.remove("_error");
   }
   function validateEmail(input) {
-
-    return !/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/.test(input.value);
-
-    
+    return !/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/.test(
+      input.value
+    );
   }
-
 });
