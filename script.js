e15fd720472731bd5b52cc21af4bd7a02fdb5314
Voviizzz <ivovochka48@gmail.com>
2021-03-09 01:00:33 +0300

    work

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("form");
   form.addEventListener("submit", formSend);
-
   async function formSend(e) {
     e.preventDefault();
     let error = formValidate(form);
